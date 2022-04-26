e78c1377a65e395fe8bfe3cd16aa22ba6af2b70d
Colin Barry <colinbarry@gmail.com>
2022-04-26 12:10:45 +0100

    feat: add `demo` branch

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,1 @@
-const path = require('path')
-const { defineConfig } = require('vite')
-
-module.exports = defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/granite.ts'),
-      name: 'Granite',
-      fileName: (format) => `granite.${format}.js`
-    },
-  }
-})
 
